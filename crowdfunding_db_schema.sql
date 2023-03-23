aee38e7f0b0dcf40a74c9532b1ded3722afc361f
saganaga <saganaga29@me.com>
2023-03-23 18:16:08 -0500

    Adds diagram & schema definition

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE category (
+    category_id VARCHAR(20) PRIMARY KEY,
+    category VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(20) PRIMARY KEY,
+    subcategory VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE contacts (
+    contact_id INT PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE campaign (
+    cf_id INT PRIMARY KEY,
+    contact_id INT NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    goal FLOAT NOT NULL,
+    pledged FLOAT NOT NULL,
+    outcome VARCHAR(20) NOT NULL,
+    bakers_count INT NOT NULL,
+    country VARCHAR(20) NOT NULL,
+    currency VARCHAR(20) NOT NULL,
+    launch_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    category_id VARCHAR(20) NOT NULL,
+    subcategory_id VARCHAR(20) NOT NULL,
+    FOREIGN KEY(contact_id) REFERENCES contacts (contact_id),
+    FOREIGN KEY(category_id) REFERENCES category (category_id),
+    FOREIGN KEY(subcategory_id) REFERENCES subcategory (subcategory_id)
+);
